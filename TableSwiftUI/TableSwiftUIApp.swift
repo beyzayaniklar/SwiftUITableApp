6c776e4306637d1893f6a0a08fead908be7ffc8d
beyzayaniklar <beyzayaniklar@gmail.com>
2026-04-06 13:14:53 -0500

    Add files via upload

---
+++ TableSwiftUI/TableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableSwiftUIApp.swift
+//  TableSwiftUI
+//
+//  Created by Yaniklar, Beyza on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
